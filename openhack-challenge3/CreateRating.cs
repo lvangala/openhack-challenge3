7e003e9c1213f6e782a2b2a1b330627741526b8e
Lokeshwar Vangala <lvangala@coca-cola.com>
2022-10-04 18:28:32 -0400

    Added code to handle create rating

--- openhack-challenge3/CreateRating.cs
+++ openhack-challenge3/CreateRating.cs
@@ -7,29 +7,55 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Openhack_Challenge3.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
-namespace openhack_challenge3
+namespace Openhack_Challenge3
 {
-    public static class CreateRating
+    public class CreateRating
     {
+        private IHttpClientFactory _httpClientFactory;
+
+        public CreateRating(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
         [FunctionName("CreateRating")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function CreateRating processed a request.");
+            log.LogInformation("CreateRating processed a request.");
 
-            string name = req.Query["name"];
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var feedback = JsonConvert.DeserializeObject<Feedback>(requestBody);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            if (await IsValidProductId(feedback.UserId) && await IsValidUserId(feedback.UserId))
+            {
+                return new OkObjectResult(JsonConvert.SerializeObject(feedback));
+            }
+            return new BadRequestObjectResult("Invalid user id or product id.  Please resubmit the request with right product id and user id");
+        }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+        private async Task<bool> IsValidProductId(string productId)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var requestUrl = $"https://serverlessohapi.azurewebsites.net/api/GetProduct?productId={productId}";
+            using var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+            using var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            return httpResponseMessage.IsSuccessStatusCode;
+        }
 
-            return new OkObjectResult(responseMessage);
+        private async Task<bool> IsValidUserId(string userId)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var requestUrl = $"https://serverlessohapi.azurewebsites.net/api/GetUser?userId={userId}";
+            using var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+            using var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            return httpResponseMessage.IsSuccessStatusCode;
         }
     }
 }
