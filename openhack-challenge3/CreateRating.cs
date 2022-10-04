f007c36ca4ff2d624474ba7d8ae052099d9eb4f6
Lokeshwar Vangala <lvangala@coca-cola.com>
2022-10-04 15:45:06 -0400

    Updated log message for CreateRating

--- openhack-challenge3/CreateRating.cs
+++ openhack-challenge3/CreateRating.cs
@@ -17,7 +17,7 @@ namespace openhack_challenge3
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function CreateRating processed a request.");
 
             string name = req.Query["name"];
 
