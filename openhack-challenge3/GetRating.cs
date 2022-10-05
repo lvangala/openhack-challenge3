48310298ced885e296845ba287f45de202872125
Rick Meyer <rick.meyer@state.mn.us>
2022-10-05 10:33:43 -0500

    adding get rating

--- openhack-challenge3/GetRating.cs
+++ openhack-challenge3/GetRating.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Openhack_Challenge3.Models;
+using Microsoft.Azure.Documents.Client;
 
 namespace openhack_challenge3
 {
@@ -14,22 +16,15 @@ namespace openhack_challenge3
     {
         [FunctionName("GetRating")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetRating/{userId}/{ratingId}")] HttpRequest req,
+            [CosmosDB(databaseName: "openhack-challenge3", collectionName: "ratings", ConnectionStringSetting = "CosmosDbConnectionString", Id = "{ratingId}", PartitionKey = "{userId}")] Feedback feedback,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var feedbackJson = JsonConvert.SerializeObject(feedback);
+            log.LogInformation("feedbackJson = " + feedbackJson);
+            return new OkObjectResult(feedbackJson);
         }
     }
 }
